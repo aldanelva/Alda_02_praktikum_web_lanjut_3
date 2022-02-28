4df80720d0ef394f5a0f058bf33cdb45978ef14b
aldanelva <aldamondel@gmail.com>
2022-02-28 23:52:11 +0700

    contact us

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactUsController;
 use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
 
@@ -48,4 +49,4 @@ Route::get('/about-us', function(){
     return view("aboutus");
 });
 
-
+Route::resource('contact-us', ContactUsController::class);
